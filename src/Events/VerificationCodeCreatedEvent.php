1f16e1490595dfdbd44907acc00dd3a2db966eb2
Chareice <chareice@live.com>
2021-08-29 16:35:53 +0800

    MOD: test

--- src/Events/VerificationCodeCreatedEvent.php
+++ src/Events/VerificationCodeCreatedEvent.php
@@ -3,5 +3,4 @@ namespace Chareice\VerificationCode\Events;
 
 class VerificationCodeCreatedEvent
 {
-
 }
--- tests/FeatureTest.php
+++ tests/FeatureTest.php
@@ -4,10 +4,18 @@
 namespace Tests;
 
 
+use Chareice\VerificationCode\Events\VerificationCodeCreatedEvent;
 use Chareice\VerificationCode\VerificationCodeService;
+use Illuminate\Support\Facades\Event;
 
 class FeatureTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Event::fake();
+    }
+
     public function testCreateCode() {
         $key = '13877776666';
 
@@ -17,7 +25,12 @@ class FeatureTest extends TestCase
         $this->assertFalse($codeService->check('123', '123'));
         $code = $codeService->setCode($key, 30);
 
+        Event::assertDispatched(VerificationCodeCreatedEvent::class, function($event, $payload) use ($key, $code) {
+            return $key === $payload['key'] && $code === $payload['code'];
+        });
+
         $this->assertFalse($codeService->check($key, '123'));
         $this->assertTrue($codeService->check($key, $code));
+
     }
 }
